76ee83b3c4dc584b3ace45f4c975edb7967bdf3e
Matthew Ferreiro <matthew.ferreiro@hotmail.com>
2015-11-06 16:42:23 -0500

    Matt_CMT_ArcherCharacterProgress

---
+++ SaviorGame/Source/SaviorGame/Weapon.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SaviorGame.h"
+#include "Weapon.h"
+
+
+// Sets default values
+AWeapon::AWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Sets the collider component to be a collider
+	Collider = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Collider"));
+
+	//Creates the root component for things to attach to
+	RootComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Root"));
+
+	//Sets the mesh component to be a mesh
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+
+	//Attaches the collider to the rootComponent(The Weapon)
+	Collider->AttachTo(RootComponent);
+	//Attaches the mesh to the RootComponent(The Weapon)
+	Mesh->AttachTo(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWeapon::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ SaviorGame/Source/SaviorGame/Weapon.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class SAVIORGAME_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeapon();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+protected:
+
+	//Adds a capsule collder to the weapon
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		UCapsuleComponent* Collider;
+
+	//Adds a mesh component to the weapon (What it looks like)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		float Damage;
+	
+};
