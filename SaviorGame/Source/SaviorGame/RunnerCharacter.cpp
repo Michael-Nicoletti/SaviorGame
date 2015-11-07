8ca504eaa86afe11d4e123ff3a6c8e5ae5e8c528
Michael-Nicoletti <michael.nicoletti@RM139-03.students.stl.yec>
2015-11-07 13:02:50 -0500

    Mike_CMT_RunnerBehavior
    Fixed camera when going from crouching to standing
    Added box pickup functionality

--- SaviorGame/Source/SaviorGame/RunnerCharacter.cpp
+++ SaviorGame/Source/SaviorGame/RunnerCharacter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SaviorGame.h"
+#include "Interactable.h"
 #include "RunnerCharacter.h"
 
 
@@ -23,6 +24,12 @@ ARunnerCharacter::ARunnerCharacter()
 	isSliding = false;
 	SlideTimer = 40.f;
 
+	//BOX PICKUP CODE//
+	Hand = CreateDefaultSubobject<USceneComponent>(TEXT("Hand"));
+	Hand->AttachTo(RootComponent);
+	Hand->RelativeLocation = FVector(100, 0, 0);
+	//BOX PICKUP CODE//
+
 }
 
 // Called when the game starts or when spawned
@@ -36,6 +43,12 @@ void ARunnerCharacter::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
 
+	FHitResult OutHit(EForceInit::ForceInit);
+	bool bTraceSuccess = TraceFromSelf(OutHit, 1000.f, ECollisionChannel::ECC_EngineTraceChannel1);
+	if (!bTraceSuccess && bIsCrouched == false)
+	{
+		GetFirstPersonCameraComponent()->SetRelativeLocation(FVector(0, 0, 64));
+	}
 
 	if (isSliding)
 	{
@@ -59,13 +72,62 @@ void ARunnerCharacter::SetupPlayerInputComponent(class UInputComponent* InputCom
 {
 	Super::SetupPlayerInputComponent(InputComponent);
 
+	InputComponent->BindAction("UseButton", IE_Pressed, this, &ARunnerCharacter::ActivateButton);
+
 	InputComponent->BindAction("Crouch", IE_Pressed, this, &ARunnerCharacter::StartCrouch);
 	InputComponent->BindAction("Crouch", IE_Released, this, &ARunnerCharacter::EndCrouch);
 
 }
 
+
+//USE BUTTON CODE//
+
+void ARunnerCharacter::ActivateButton()
+{
+	//Create an array to hold all of the overlapping actors on the player
+	TArray<AActor*> OverlappingActors;
+
+	GetOverlappingActors(OverlappingActors, AInteractable::StaticClass());
+
+	//Create an interactable pointer to hold the closest object at the end of it as well as a float to hold the closest distance
+	AInteractable *ClosestObject = nullptr;
+
+	//@note: FLT_MAX just sets the float to be the higest number possible for a float
+	float ClosestObjectDist = FLT_MAX;
+
+	//Loop that checks every object in the array to find what the closest object is
+	for (int i = 0; i < OverlappingActors.Num(); i++)
+	{
+		float ObjectDistance = FVector::Dist(this->GetActorLocation(), OverlappingActors[i]->GetActorLocation());
+
+		if (ObjectDistance < ClosestObjectDist)
+		{
+			ClosestObjectDist = ObjectDistance;
+			ClosestObject = Cast<AInteractable>(OverlappingActors[i]);
+		}
+	}
+
+	if (ClosestObject != nullptr)
+	{
+		ClosestObject->Interact(this);
+
+	}
+}
+
+//USE BUTTON CODE END//
+
+
+USceneComponent* ARunnerCharacter::GetHand()
+{
+	return Hand;
+}
+
+
+//CROUCH AND SLIDE CODE//
+
 void ARunnerCharacter::StartCrouch()
 {
+	HitHead = true;
 	GetFirstPersonCameraComponent()->SetRelativeLocation(FVector(0, 0, 25));
 	this->Crouch();
 	if (this->GetVelocity().Size() > 600.f)
@@ -84,9 +146,34 @@ void ARunnerCharacter::EndCrouch()
 	this->GetCharacterMovement()->MaxWalkSpeedCrouched = baseCrouchMoveSpeed;
 	this->GetCharacterMovement()->GroundFriction = 8;
 	this->GetCharacterMovement()->BrakingFriction = 0;
-	this->GetCharacterMovement()->BrakingDecelerationWalking = baseBrakeDeceleration;
-	GetFirstPersonCameraComponent()->SetRelativeLocation(FVector(0, 0, 64));
+	this->GetCharacterMovement()->BrakingDecelerationWalking = baseBrakeDeceleration;	
 	isSliding = false;
 	SlideTimer = 40.f;
+}
+
+//CROUCH AND SLIDE CODE//
+
+bool ARunnerCharacter::TraceFromSelf(FHitResult& OutResult, const float TraceDistance, ECollisionChannel const CollisionChannel)
+{
+	FVector PlayerLocation = this->GetActorLocation();
+
+	//GetPlayerViewPoint is used to give the origin and direction
+	//of the trace from the player
+
+	FVector const StartTrace = PlayerLocation;
+	FVector const TraceDirection = PlayerLocation.UpVector;
+	FVector const EndTrace = StartTrace + TraceDirection*TraceDistance;
+
+	//Struct that defines parameters into collision function
+	FCollisionQueryParams TraceParams(FName(TEXT("TraceFromSelf")), true, this);
+
+	bool bHitReturned = false;
+	UWorld* const World = GetWorld();
+
+	if (World)
+	{
+		bHitReturned = World->LineTraceSingleByChannel(OutResult, StartTrace, EndTrace, CollisionChannel, TraceParams);
+	}
+	return bHitReturned;
 }
 
